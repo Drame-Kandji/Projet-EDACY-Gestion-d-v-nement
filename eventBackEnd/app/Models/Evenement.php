ffeb54c6ea89f1b809a2177670ad1c1cd58cd820
Aliou Dramé <aliou.drame@univ-thies.sn>
2025-03-31 22:42:57 +0100

    endpoints des evenements

---
+++ eventBackEnd/app/Models/Evenement.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Evenement extends Model
+{
+    use hasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+        'location',
+        'image',
+        'heure',
+        'category',
+        'attendees'
+
+    ];
+}
