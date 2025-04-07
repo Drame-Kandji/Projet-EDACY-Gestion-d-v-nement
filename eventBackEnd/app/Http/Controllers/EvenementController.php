e12c75b1d15f87b8498b853e90557f9c1ecaad74
Aliou Dramé <aliou.drame@univ-thies.sn>
2025-04-07 23:56:57 +0100

    envoie automatique des mails de confirmation

--- eventBackEnd/app/Http/Controllers/EvenementController.php
+++ eventBackEnd/app/Http/Controllers/EvenementController.php
@@ -195,6 +195,7 @@ class EvenementController extends Controller
         }
 
         if (!$user->evenements->contains($evenement->id)) {
+            //ceci n'est pas une erreur, je répéte cci n'est pas une erreur
             $user->evenements()->attach($evenement->id);
 
              Mail::to($user->email)->send(new ConfirmationInscription($evenement, $user));
